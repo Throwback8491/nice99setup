3aaad6a2a964412ef9e67bf346a9d49c4f3e38ba
davidw <dw.c4u5x@silomails.com>
2023-12-01 22:51:28 +1300

    update libs to use and remove annoying testthat buggy that causes renv::install() to fail

--- R/addin_libs.R
+++ R/addin_libs.R
@@ -1,1 +1,3 @@
-# renv::install(c("datapasta", "sinew", "styler"))
+# renv::install(c("datapasta", "sinew", "styler", "tidyverse"))
+# devtools::install_github("lionel-/codegrip")
+# devtools::install_github("dracodoc/mischelper")
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(nice99setup)
+# library(nice99setup)
 
 test_check("nice99setup")
